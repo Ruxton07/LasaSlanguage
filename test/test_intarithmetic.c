b90b3056c0320d46658eacaee0f409119bd9fde8
Ryan <rt.kellar@gmail.com>
2025-02-11 11:23:35 -0600

    Created int arithmetic tests

---
+++ test/test_intarithmetic.c
@@ -0,0 +1,36 @@
+#include "test_intarithmetic.h"
+#ifdef TEST_INTARITHMETIC_H
+
+#include "unity.h"
+
+#include "lexer.h"
+
+void setUp(void)
+{
+}
+
+void tearDown(void)
+{
+}
+
+void test_modulus(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement integer modulus");
+}
+
+void test_increment(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement integer increment");
+}
+
+void test_decrement(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement integer decrement");
+}
+
+void test_exponentiation(void)
+{
+    TEST_IGNORE_MESSAGE("Need to Implement integer exponentiation");
+}
+
+#endif // TEST_INTARITHMETIC_H
